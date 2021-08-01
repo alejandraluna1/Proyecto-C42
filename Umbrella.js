182c0e5594edbdf4e6225c79e9056e8462de2ec7
alejandraluna1 <81326858+alejandraluna1@users.noreply.github.com>
2021-08-01 18:13:24 -0500

    Update Umbrella.js

--- Umbrella.js
+++ Umbrella.js
@@ -7,14 +7,14 @@ class Umbrella {
         this.umbrella = Bodies.circle(x,y,50,options);
         this.radius = 50;
         World.add(world, this.umbrella)
-        //load Image for BestMan
+        //cargar la imagen del Mejor Hombre
         
     }
 
     display(){
         var pos = this.umbrella.position;
         imageMode(CENTER);
-        //display the image for BestMan if the frameCount is over 200, otherwise display the boy with umbrella image
+        //mostrar la imagen del Mejor Hombre si el conteo de cuadros es más de 200, sino, mostrar al niño con la imagen de la sombrilla
         image(this.image,pos.x,pos.y+70,300,300);
     }
 }
